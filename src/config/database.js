67fdbca636e26f0a02cc1da3f0da027f12524b02
Hridyansh Shukla <hridyanshshukla0@gmail.com>
2026-05-11 10:31:16 +0530

    DB URL change

--- src/config/database.js
+++ src/config/database.js
@@ -1,11 +1,19 @@
 require("dotenv").config();
 const { Pool } = require("pg");
 
+// const pool = new Pool({
+//   host: process.env.DB_HOST || "localhost",
+//   port: parseInt(process.env.DB_PORT) || 5432,
+//   database: process.env.DB_NAME || "kanpur_complaint",
+//   user: process.env.DB_USER || "postgres",
+//   password: "abc123",
+//   max: 20,
+//   idleTimeoutMillis: 30000,
+//   connectionTimeoutMillis: 2000,
+// });
+
 const pool = new Pool({
-  connectionString: "postgresql://nagar_nigam_db_user:gvNZWc8Oz5qGBGfDjU2peJmMLycTHGB0@dpg-d7uu0cnlk1mc73apincg-a/nagar_nigam_db",
-  ssl: {
-    rejectUnauthorized: false, // For development only; in production, use proper SSL certs
-  },
+  connectionString: "postgresql://hridyansh_shukla_user:Dg04AZ1JwWRct03E36iItEtTYVFccmev@dpg-d7vdmahj2pic73ed5drg-a/hridyansh_shukla",
 });
 
 pool.on("connect", () => {
